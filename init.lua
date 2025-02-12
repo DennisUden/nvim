56ba2edb5b1c07f4536d0df4a56b2cbff7628d0e
DennisUden <162509535+DennisUden@users.noreply.github.com>
2025-02-12 15:31:57 +0100

    colorscheme vim for windows only

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 vim.wo.relativenumber = true
 vim.api.nvim_set_option("clipboard","unnamed")
 vim.opt.ignorecase = true
-vim.cmd [[colorscheme vim]]
-
+if (jit.os == "Windows") then 
+	vim.cmd [[colorscheme vim]]
+end
